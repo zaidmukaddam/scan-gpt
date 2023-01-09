f730f8a5008f7c344233c3f50e3f5f6adaab27df
Zaid Mukaddam <zaidaltafmukaddam@gmail.com>
2023-01-09 14:09:54 +0530

    Add Google Analytics

--- scan-gpt-next/pages/_document.tsx
+++ scan-gpt-next/pages/_document.tsx
@@ -1,10 +1,25 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <script async src="https://cdn.splitbee.io/sb.js" />
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-D5TSBR0CSG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-D5TSBR0CSG');
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
